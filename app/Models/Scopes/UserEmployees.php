7f92b3340c2e87b86c785c78ff01367a921e46e1
Aqlan Al-neamah <alneamahaqlan@gmail.com>
2025-02-17 19:24:49 +0300

    Update UserEmployees.php

--- app/Models/Scopes/UserEmployees.php
+++ app/Models/Scopes/UserEmployees.php
@@ -14,12 +14,19 @@ class UserEmployees implements Scope
      */
     public function apply(Builder $builder, Model $model): void
     {
-        $user=Auth::user();
-        if( $user?->isSuperAdmin()??false) {
-            $builder;
+        $user = Auth::user();
+        $guard = Auth::getDefaultDriver();
+
+        if ($guard === 'web') {
+            if( $user?->isSuperAdmin()??false) {
+                $builder;
+            }
+            if($user?->isAdmin()??false) {
+                $builder->where('user_id', $user->id);
+            }
         }
-        if($user?->isAdmin()??false) {
-            $builder->where('user_id', $user->id);
+        if ($guard === 'emp') {
+            $builder->where('user_id', $user->user_id);
         }
 
     }
