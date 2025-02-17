c42a76b8a76fcbb4fbd4822c48ddbc4724625c97
Aqlan Al-neamah <alneamahaqlan@gmail.com>
2025-02-17 21:50:32 +0300

    TaskFollowUpResource

--- app/Filament/App/Resources/TaskFollowUpResource.php
+++ app/Filament/App/Resources/TaskFollowUpResource.php
@@ -33,7 +33,6 @@ class TaskFollowUpResource extends Resource
         return $form
             ->schema([
                 Forms\Components\Select::make('task_id')
-                    ->label('Task')
                     ->options(function () use ($userId) {
                         return Task::whereHas('project', function (Builder $query) use ($userId) {
                             $query->where('user_id', $userId);
@@ -45,58 +44,40 @@ class TaskFollowUpResource extends Resource
                 Forms\Components\Select::make('task_status_id')
                     ->relationship('taskStatusForUser', 'name')
                     ->required()
-                    ->reactive() // لجعل القائمة التفاعلية
+                    ->reactive()
                     ->afterStateUpdated(function (callable $set, $state) {
                         $taskStatus = TaskStatus::find($state);
-                        $set('is_completed', $taskStatus && $taskStatus->is_completely); // تعيين إذا كانت الحالة مكتملة
+                        $set('is_completed', $taskStatus && $taskStatus->is_completely);
                     }),
 
                 Forms\Components\Textarea::make('note')
                     ->columnSpanFull(),
 
                 Forms\Components\TextInput::make('exact_time')
-                    ->label('Exact Time (minutes)')
                     ->numeric()
                     ->minValue(0)
-                    ->required(fn (callable $get) => $get('is_completed')) // إجبارية الحقل إذا كانت المهمة مكتملة
-                    ->hidden(fn (callable $get) => !$get('is_completed')) // إخفاء الحقل إذا لم تكن الحالة مكتملة
-                    ->reactive(), // لجعل الحقل يتفاعل مع التغييرات
+                    ->required(fn (callable $get) => $get('is_completed'))
+                    ->hidden(fn (callable $get) => !$get('is_completed'))
+                    ->reactive(),
             ])
-            ->statePath('data'); // للتأكد من عدم تأثير الحالة المؤقتة على البيانات
+            ->statePath('data');
     }
-
-
-
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                // Tables\Columns\TextColumn::make('emp_id')
-                //     ->numeric()
-                //     ->sortable(),
-                // Tables\Columns\TextColumn::make('task_id')
-                //     ->numeric()
-                //     ->sortable(),
-                // Tables\Columns\TextColumn::make('task_status_id')
-                //     ->numeric()
-                //     ->sortable(),
-                Tables\Columns\TextColumn::make('emp.name')  // Assuming relationship is defined in TaskFollowUp model
-                    ->label('Employee')
+                Tables\Columns\TextColumn::make('emp.name')
                     ->sortable(),
-                Tables\Columns\TextColumn::make('task.title') // Assuming relationship is defined in TaskFollowUp model
-                    ->label('Task Title')
+                Tables\Columns\TextColumn::make('task.title')
                     ->sortable(),
-                    Tables\Columns\TextColumn::make('note') // Assuming relationship is defined in TaskFollowUp model
-                    ->label('Note')
+                Tables\Columns\TextColumn::make('note')
                     ->sortable(),
-                Tables\Columns\TextColumn::make('taskStatus.name') // Assuming relationship is defined in TaskFollowUp model
-                    ->label('Task Status')
+                Tables\Columns\TextColumn::make('taskStatus.name')
                     ->sortable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable(),
-                    // ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('updated_at')
                     ->dateTime()
                     ->sortable()
@@ -106,43 +87,10 @@ class TaskFollowUpResource extends Resource
                 //
             ])
             ->actions([
-                // Tables\Actions\EditAction::make()
-                // ->mutateFormDataUsing(function (array $data): array {
-                //     // dd(auth()->guard('emp')->user());
-                //     if (auth()->guard('emp')->user()->is_admin == 0) {
-
-                //         $task_status = TaskStatus::where('id', $data['task_status_id'])->first();
-                //         // dd($task_status);
-                //         if ($task_status->only_for_admin == true) {
-                //             Notification::make()
-                //                 ->title('ليس لديك صلاحية')
-                //                 ->danger()
-                //                 ->send();
-
-                //             throw ValidationException::withMessages([
-                //                     'task_status_id' => 'ليس لديك صلاحية لتعيين هذا الحالة.',
-                //             ]);
-                //         }
-
-                //     }
-                //     $data['emp_id'] = auth()->guard('emp')->id();
-                //     $phone_main = '966571718153';
-                //     $auth = '40703bb7812b727ec01c24f2da518c407342559c';
-                //     $profileId = 'aedd0dc2-8453';
-                //     $phone = $phone_main.'@c.us';
-                //     $message = 'test message';
-
-                //     $response = WhatsAppService::send_with_wapi($auth, $profileId, $phone, $message);
-
-
-
-                //     return $data;
-                // }),
+                //
             ])
             ->bulkActions([
-                // Tables\Actions\BulkActionGroup::make([
-                //     Tables\Actions\DeleteBulkAction::make(),
-                // ]),
+                //
             ]);
     }
 
@@ -158,22 +106,19 @@ class TaskFollowUpResource extends Resource
         return [
             'index' => Pages\ListTaskFollowUps::route('/'),
             'create' => Pages\CreateTaskFollowUp::route('/create'),
-            // 'edit' => Pages\EditTaskFollowUp::route('/{record}/edit'),
         ];
     }
 
     public static function getEloquentQuery(): Builder
     {
         $userId = auth()->guard('emp')->user()->user_id;
-        // return TaskFollowUp::whereHas('task.emp_project2', function (Builder $query) use ($userId) {
-        //     $query->where('user_id', $userId);
-        // });
         return TaskFollowUp::whereHas('task', function (Builder $query) use ($userId) {
             $query->whereHas('project', function (Builder $query) use ($userId) {
                 $query->where('user_id', $userId);
             });
         });
     }
+
     public static function getModelLabel(): string
     {
         return ModelLabelHelper::getModelLabel(static::$model);
