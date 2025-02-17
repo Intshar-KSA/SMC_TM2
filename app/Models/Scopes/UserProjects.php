216c72944b59766704f0b91531cf81d82db8ae3a
Aqlan Al-neamah <alneamahaqlan@gmail.com>
2025-02-17 18:54:35 +0300

    Update UserProjects.php

--- app/Models/Scopes/UserProjects.php
+++ app/Models/Scopes/UserProjects.php
@@ -11,13 +11,21 @@ class UserProjects implements Scope
 {
     public function apply(Builder $builder, Model $model): void
     {
-        $user=Auth::user();
-        if( $user?->isSuperAdmin()??false) {
-            $builder;
+        $user = Auth::user();
+        $guard = Auth::getDefaultDriver();
+
+        if ($guard === 'web') {
+            if( $user?->isSuperAdmin()??false) {
+                $builder;
+            }
+            if($user?->isAdmin()??false) {
+                $builder->where('user_id', $user->id);
+            }
         }
-        if($user?->isAdmin()??false) {
-            $builder->where('user_id', $user->id);
+        if ($guard === 'emp') {
+            $builder->where('user_id', $user->user_id);
         }
+
 
     }
 }
